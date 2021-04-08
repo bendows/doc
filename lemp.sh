91dd212188b5e6ebbbce30a9f2239084c2dac0bc
Ben <993283+bendows@users.noreply.github.com>
2021-04-09 00:12:47 +0200

    Update lemp.sh

--- lemp.sh
+++ lemp.sh
@@ -5,4 +5,6 @@ sudo apt install -y mysql-server
 sudo apt install -y php-fpm php-mysql
 systemctl enable php7.4-fpm
 systemctl enable nginx
+apt install -y certbot
+
 # sudo mysql_secure_installation
