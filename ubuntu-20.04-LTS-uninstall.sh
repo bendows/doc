81b329c0bbb69b1c7ca4e311265900e09a9ac57b
Ben <993283+bendows@users.noreply.github.com>
2021-04-08 23:11:04 +0200

    Update ubuntu-20.04-LTS-uninstall.sh

--- ubuntu-20.04-LTS-uninstall.sh
+++ ubuntu-20.04-LTS-uninstall.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -x
-sudo apt-get -y --force-yes remove policykit-1
+sudo apt-get --allow-remove-essential -y remove policykit-1
+sudo apt-get --allow-remove-essential -y remove accounts-daemon
+sudo apt-get --allow-remove-essential -y remove dbus
+sudo apt-get --allow-remove-essential -y remove multipath-tools
+sudo apt autoremove
 sudo systemctl stop snapd && sudo systemctl disable snapd
 sudo systemctl stop snapd && sudo systemctl disable snapd
-sudo apt -y --force-yes purge snapd
+sudo apt --allow-remove-essential -y purge snapd
 sudo rm -rf /home/ubuntu/snap /snap /var/snap /var/lib/snapd /var/cache/snapd /usr/lib/snapd
-sudo apt-get -y --force-yes remove accounts-daemon
-apt-get -y --force-yes remove dbus
-apt-get -y --force-yes remove multipath-tools
-sudo -y --force-yes apt autoremove
